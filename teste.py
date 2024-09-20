69e61f421369d6a3cbe04efdd89d4f2f8f6feee1
Hugo067 <hugohenriquesousafreres@gmail.com>
2024-09-20 17:06:47 -0400

    first commit

---
+++ teste.py
@@ -0,0 +1,104 @@
+# print ("hello world!!!")
+# print(2+3)
+# print('dia','mes','ano',sep='/')
+# print(type(3.14))
+
+
+
+
+# print(3.14)
+# print(42000)
+# print(42,000)
+# print(42.000)
+# print(type(42000))
+# print(type(42.000))
+# print(type(42))
+
+
+
+
+# print(type("5"+"5"))# string(texto) nao conta como numero
+# print(type)# type mostra qual a variavel 
+# n= inteiro
+# pi = real
+# string = texto
+
+
+
+
+# tipo = True
+# pi=0
+# print(type(pi))
+# print(type(True))
+
+
+
+
+
+# print(81**0.5)#conto sempre vai ser pela direita pela direita
+
+
+
+# peso = 80
+# altura = 1.89
+# print(80/1.89)
+
+
+
+# input = leia
+
+
+# nome = input("digite seu nome = ")
+# print("seu nome é",nome)
+
+
+# idade=int(input("digite sua idade = "))
+# print(type(idade))
+# salario = int(input("salario?"))
+# imposto = float(input("digite o imposto = "))    preciso informar seu tipo (int, float)antes do input
+
+
+
+# salario = float(input("digite seu salario = "))
+# print (salario)
+
+
+
+# %d = int
+# %s = string
+# %f = float
+# %b = bool
+
+# sempre ira colocar pela ordem ()
+
+
+
+# salario = float(input("salario = "))
+# print("o salario digitado foi %f", salario)
+
+
+
+
+# a = "Hugo"
+# b = "Henrique"
+# print("prezado "+a+" "+b+"."+" olá!")
+
+
+
+#print(""+10*"+"+"")
+
+
+
+# frase = 'triangulo de base igual a {0} e altura igual {1} possui área igual a {2}.'.format(3,4,12)
+# print(frase)
+
+
+
+# linguagem = "python"
+# f"progamando em {linguagem}"
+# print(f"progamando em {linguagem}")
+
+x = (2/3)
+print(f"printando o {x}")
+print( f"printando o {x:.2f}")
+
