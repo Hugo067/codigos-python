1133e1debc8083933a5a74eff16912e0e49aab10
Hugo067 <hugohenriquesousafreres@gmail.com>
2024-10-03 17:03:55 -0400

    aula do dia 03.10.2024

--- lista.py
+++ lista.py
@@ -48,3 +48,84 @@ print(uma_lista[:4])
 print(uma_lista[3:])
 print(uma_lista[:])
 print(uma_lista[0:6])
+
+b = [1, 2, 1, 5, 6, 4, 3, 1]
+
+# uma maneira de excluir
+
+uma_lista1 = ["a", "b", "c", "d", "e", "f"]
+print(uma_lista1)
+print(len(uma_lista1))
+
+uma_lista1[1:3] = []
+print(uma_lista1)
+print(len(uma_lista1))
+
+#adicionar
+uma_lista2 = ["a", "b", "f"]
+uma_lista2[1:1] = ["b", "c"]
+print(uma_lista2)
+uma_lista2[4:4] = ["e"]
+print(uma_lista2)
+
+#deletar
+
+c = ["um", "dois", "três"]
+del c[1]
+print(c)
+
+lista = ["a", "b", "c", "d", "e", "f"]
+del lista [1:5]
+print(lista)
+
+#inserir elemento
+d = [81, 82, 83]
+d.append(5)
+print (d)
+
+#ordenar crescente
+e = [88, 81, 82, 90]
+e.sort()
+print (e)
+
+#ordenar decrescente
+
+e = [88, 81, 82, 90]
+e.sort(reverse = True)
+print (e)
+
+#indice do elemento que eu quero
+
+f =[1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(f.index (4))
+
+#inserir na posiçao que eu quero
+
+g = [81, 82, 88, 90]
+g.insert(0,100)
+print(g)
+
+#quantas vezes o elemento aparece
+h = [82, 88, 88, 88, 84, 88, 88, 88, 88, 88, 88, 88, 80]
+print(h)
+print(h.count(88))
+
+#excluir o elemento que eu quero
+i = [82, 88, 88, 88, 84, 88, 88, 88, 88, 88, 88, 88, 80]
+i.pop()
+print(i)
+
+i.pop(0)
+print(i)
+
+#extand ele unifica a lista
+
+j = [1,2,3]
+j.extend([4,5])
+print(j)
+
+
+frutas2 = ["maca", "laranja", "banana", "cereja", "uva"]
+print(frutas2)
+frutas2 [1] = "goiaba"
+print(frutas2)
