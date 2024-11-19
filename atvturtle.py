e069d47da1e2d99eaabd00af8b9756ecf4b7ed8d
Hugo067 <hugohenriquesousafreres@gmail.com>
2024-11-19 15:13:41 -0400

    atv pyautogui

--- atvturtle.py
+++ atvturtle.py
@@ -1,8 +1,22 @@
-import turtle
+# import turtle
 
-for i in range(4):
+# for i in range(4):
     
-    turtle.forward(100)
-    turtle.right(90)
+#     turtle.forward(100)
+#     turtle.right(90)
 
-turtle.done()
+# turtle.done()
+
+#--------------------------------
+import pyautogui
+
+pyautogui.moveTo(419,499)
+pyautogui.click
+
+# import pyautogui
+# import time
+
+# for i in range(2):
+#     time.sleep(4)
+#     posicao = pyautogui.position()
+#     print(posicao)
