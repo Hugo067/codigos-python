cc8bc24cf79e9e82ece4d7c435c227d5f06d4ca0
Hugo067 <hugohenriquesousafreres@gmail.com>
2024-11-19 14:26:40 -0400

    atv turtle

--- lista02/ex27.py
+++ lista02/ex27.py
@@ -1,12 +1,22 @@
 
+# def esta_ordenada(lista):
+#     return all(lista[i] <= lista[i + 1] for i in range(len(lista) - 1))
+
+
+# entrada = input("Digite os números da lista separados por espaço: ")
+
+
+# lista = list(map(int, entrada.split()))
+
+
+# print(esta_ordenada(lista))
+
 def esta_ordenada(lista):
-    return all(lista[i] <= lista[i + 1] for i in range(len(lista) - 1))
+    for i in range(len(lista) - 1):
+        if lista[i] > lista[i + 1]:
+            return False  
+    return True  
 
-# Solicitando a entrada do usuário
-entrada = input("Digite os números da lista separados por espaço: ")
-
-# Convertendo a entrada em uma lista de inteiros
-lista = list(map(int, entrada.split()))
-
-# Verificando se a lista está ordenada e imprimindo o resultado
-print(esta_ordenada(lista))
+lista = [1, 2, 3, 4, 5]
+resultado = esta_ordenada(lista)
+print("A lista está ordenada em ordem crescente? {resultado}")
